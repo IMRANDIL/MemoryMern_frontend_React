c13ac0dd20885e37500174a192bcfeb86af835e1
IMRANDIL <aliimranadil2@gmail.com>
2022-05-16 11:00:12 +0530

    yh

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -21,10 +21,6 @@ const Form = () => {
 
 
 
-    const handleInput = (e) => {
-        setPostData({ ...postData, creator: e.target.value })
-    }
-
 
 
     return (
@@ -33,10 +29,10 @@ const Form = () => {
                 <Typography variant='h6'>
                     Creating a Memory
                 </Typography>
-                <TextField name="creator" variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={handleInput} />
-                <TextField name="title" variant='outlined' label='Title' fullWidth value={postData.title} onChange={handleInput} />
-                <TextField name="message" variant='outlined' label='Message' fullWidth value={postData.message} onChange={handleInput} />
-                <TextField name="tags" variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={handleInput} />
+                <TextField name="creator" variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+                <TextField name="title" variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField name="message" variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <TextField name="tags" variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
             </form>
         </Paper>
     )
