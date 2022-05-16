723143e64513cf0bf30c399869bf28ec765918d5
IMRANDIL <aliimranadil2@gmail.com>
2022-05-16 10:23:17 +0530

    yh

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useStyles from './styles';
-
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
 
 
 
 const Form = () => {
 
     const classes = useStyles();
+    const [postData, setPostData] = useState({
+        creator: '',
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: ''
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
+
+    const handleInput = () => {
+
+    }
 
 
 
     return (
-        <div>Form</div>
+        <Paper className={classes.paper}>
+            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant='h6'>
+                    Creating a Memory
+                </Typography>
+                <TextField name="creator" variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={handleInput} />
+            </form>
+        </Paper>
     )
 }
 
