e8dd70cf661d95349c1347045a593687949d338b
IMRANDIL <aliimranadil2@gmail.com>
2022-05-16 08:37:07 +0530

    yh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import { Provider } from 'react-redux';
 import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 
 ReactDom.render(<App />, document.getElementById('root'));
